f3534a734b983c9ace9ade59825d08360cfe7c2b
BaoNQ <nqbao2712@gmail.com>
2024-02-06 14:14:12 +0700

    Feat: [LOGI] add updated and add new employee to phase for project

--- ProjectManagementApp/PMApp.API/Services/ProjectServices/ProjectService.cs
+++ ProjectManagementApp/PMApp.API/Services/ProjectServices/ProjectService.cs
@@ -13,11 +13,15 @@ namespace PMApp.API.Services
     public class ProjectService : IProjectService
     {
         public ProjectService() {
-            const string jsonFilePath = "./Services/DataSeeder/Project.DataSeeder.json";
+            string jsonFilePath = "./Services/DataSeeder/Project.DataSeeder.json";
             string jsonData = File.ReadAllText(jsonFilePath);
             _initData = JsonConvert.DeserializeObject<List<Project>>(jsonData);
+            jsonFilePath = "./Services/DataSeeder/Employee.DataSeeder.json";
+            jsonData = File.ReadAllText(jsonFilePath);
+            _initEmployee = JsonConvert.DeserializeObject<List<Employee>>(jsonData);
         }
         private List<Project> _initData;
+        private List<Employee> _initEmployee;
         public ListOfProject GetProjects(ProjectFilter filter)
         {
             filter ??= new();
@@ -52,17 +56,56 @@ namespace PMApp.API.Services
                 returnedData.Message = "The project already exists in the system.";
                 return returnedData;
             }
-            bool invalidPhaseCode = projectData.ProjectPhases.Any(pPhase => !PMAppConstant.PMAppPhases.ContainsKey(pPhase.PhaseCode));
-            if(invalidPhaseCode)
+            if (projectData.ProjectPhases != null)
             {
-                returnedData.StatusCode = 400;
-                returnedData.Message = "Invalid input";
-                return returnedData;
+                bool invalidPhaseCode = projectData.ProjectPhases.Any(pPhase => !PMAppConstant.PMAppPhases.ContainsKey(pPhase.PhaseCode));
+                if(invalidPhaseCode)
+                {
+                    returnedData.StatusCode = 400;
+                    returnedData.Message = "Invalid input";
+                    return returnedData;
+                }
             }
             Project projectDataSubmit = new(projectData);
             _initData.Add(projectDataSubmit);
             returnedData.StatusCode = 201;
             returnedData.ResponseData.Add("ProjectId", $@"{projectDataSubmit.Id}");
+            return returnedData;
+        }
+        public ResponseInfo UpdateProject(ProjectDTO projectData, Guid projectId)
+        {
+            ResponseInfo returnedData = new();
+            var existingProject = _initData.FirstOrDefault(project => project.Id == projectId);
+            if (existingProject == null)
+            {
+                returnedData.StatusCode = 404;
+                returnedData.Message = "Project not found";
+                return returnedData;
+            }
+            var duplicateNameProject = _initData.FirstOrDefault(project => project.Id != projectId && project.ProjectName == projectData.ProjectName);
+            if (duplicateNameProject != null)
+            {
+                returnedData.StatusCode = 409;
+                returnedData.Message = "The project already exists in the system.";
+                return returnedData;
+            }
+            if (projectData.ProjectPhases != null)
+            {
+                bool invalidPhaseCode = projectData.ProjectPhases.Any(pPhase => !PMAppConstant.PMAppPhases.ContainsKey(pPhase.PhaseCode));
+                if(invalidPhaseCode)
+                {
+                    returnedData.StatusCode = 400;
+                    returnedData.Message = "Invalid input";
+                    return returnedData;
+                }
+            }
+            existingProject.ProjectName = projectData.ProjectName;
+            existingProject.Description = projectData.Description;
+            existingProject.CustomerName = projectData.CustomerName;
+            existingProject.StartDate = projectData.StartDate;
+            existingProject.EndDate = projectData.EndDate;
+            existingProject.ProjectPhases = projectData.ProjectPhases;
+            returnedData.Message = "Editted";
             return returnedData;
         }
         public ResponseInfo DeleteProject(Guid projectId)
@@ -79,6 +122,50 @@ namespace PMApp.API.Services
                 _initData.Remove(projectToRemove);
                 returnedData.Message = "Deleted";
             }
+            return returnedData;
+        }
+        public ResponseInfo AddEmployee(EmployeeAddedInfo employeeAdded, Guid projectId)
+        {
+            ResponseInfo returnedData = new();
+            var existingProject = _initData.FirstOrDefault(project => project.Id == projectId);
+            if (existingProject == null)
+            {
+                returnedData.StatusCode = 404;
+                returnedData.Message = "Project not found";
+                return returnedData;
+            }
+            var existingEmployee = _initEmployee.FirstOrDefault(emp => emp.Id == employeeAdded.EmployeeId);
+            if(existingEmployee == null)
+            {
+                returnedData.StatusCode = 400;
+                returnedData.Message = "Invalid input";
+                returnedData.ResponseData.Add("EmployeeId", "Not Found");
+                return returnedData;
+            }
+            if(!PMAppConstant.PMAppPhases.ContainsKey(employeeAdded.PhaseCode))
+            {
+                returnedData.StatusCode = 400;
+                returnedData.Message = "Invalid input";
+                returnedData.ResponseData.Add("PhaseCode", "Invalid");
+                return returnedData;
+            }
+            var projectPhase = existingProject.ProjectPhases.FirstOrDefault(pPhase => pPhase.PhaseCode == employeeAdded.PhaseCode);
+            if(projectPhase == null)
+            {
+                returnedData.StatusCode = 400;
+                returnedData.Message = "Invalid input";
+                returnedData.ResponseData.Add("PhaseCode", "Not Found");
+                return returnedData;
+            }
+            var existingEmployeeInPhase = projectPhase.Employees.FirstOrDefault(emp => emp.Id == employeeAdded.EmployeeId);
+            if(existingEmployeeInPhase != null)
+            {
+                returnedData.StatusCode = 409;
+                returnedData.Message = "The employee already exists in the project.";
+                return returnedData;
+            }
+            projectPhase.Employees.Add(existingEmployee);
+            returnedData.Message = "Added";
             return returnedData;
         }
     }
